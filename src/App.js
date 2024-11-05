99e28799c6aa439a6e2b22ec3d4f324d08b59a8e
Adil <adilosama1@gmail.com>
2024-11-06 01:30:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider, useAuth } from "./AuthContext";
+import Signup from "./Signup";
+import Login from "./Login";
+import Dashboard from "./Dashboard";
+import AdminDashboard from "./AdminDashboard";
+import ChartComponent from "./ChartComponent"; // Import your ChartComponent
+import NavBar from "./NavBar";
+import ProtectedRoute from "./ProtectedRoute";
+
+const DefaultRoute = () => {
+  const { user } = useAuth();
+  return user ? <Navigate to="/chart" /> : <Navigate to="/login" />;
+};
+
+const AdminProtectedRoute = ({ children }) => {
+  const { role } = useAuth();
+  return role === "admin" ? children : <Navigate to="/dashboard" />;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<DefaultRoute />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin-dashboard"
+            element={
+              <AdminProtectedRoute>
+                <AdminDashboard />
+              </AdminProtectedRoute>
+            }
+          />
+          <Route
+            path="/chart" // Add route for ChartComponent
+            element={
+              <ProtectedRoute>
+                <ChartComponent />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
